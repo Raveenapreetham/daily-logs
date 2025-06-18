5339431b99948d058dcdc5f9138520c6474700c5
Raveenapreetham <raveenapreethamr@gmail.com>
2025-06-18 15:46:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello")
+Class Name:
+
+def __init__(self,name, age, dept):
+     self.name = name
+     self.age= age
+     self.dept = dept
+
+     def updatename(self,name):
+          self.
